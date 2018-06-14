e9b5840e826f611e7c5c01e9f47f1c0735aad41c
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-14 03:09:38 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', 'WelcomeController@index');
+
+// Register authentication
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// Login authentication
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
