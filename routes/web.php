81c30f5ba4f45c0584a5bbd3306c79c2f34730aa
takumi.hashimoto <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-06-15 04:55:57 +0000

    ranking

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+// Ranking
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
@@ -30,3 +33,4 @@ Route::group(['middleware' => ['auth']], function () {
     Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     Route::resource('users', 'UsersController', ['only' => ['show']]);
 });
+
